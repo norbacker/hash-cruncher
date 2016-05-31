8168a7759823648a742850154fff14ff186a5de8
Gustav Norbäcker <gustav.norbacker@ffcg.se>
2016-05-31 16:14:52 +0200

    Added info about instances and environment.

--- src/main/java/se/ffcg/pub/hash/HtmlController.java
+++ src/main/java/se/ffcg/pub/hash/HtmlController.java
@@ -2,6 +2,8 @@ package se.ffcg.pub.hash;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HtmlController {
+	
+	@Autowired
+	Environment env;
 
 	@RequestMapping(path = "/crunch/{prefix}/{zeroes}", produces = "text/html")
 	public String crunchHtml(@PathVariable("prefix") String prefix, @PathVariable("zeroes") int zeroes,
@@ -21,7 +26,9 @@ public class HtmlController {
 		model.addAttribute("verbose", verbose);
 		model.addAttribute("hash", cruncher.hash(response));
 		model.addAttribute("time", (System.nanoTime() - startTime) / 1000000);
-
+		model.addAttribute("instance", env.getProperty("HOSTNAME", "") + " - " + env.getProperty("JAVA_APP_JAR", ""));
+		model.addAttribute("production", env.getProperty("KUBERNETES_NAMESPACE", "").toUpperCase().contains("PRODUCTION"));
+		
 		return "crunch";
 	}
 }
